eed85cdb733d54b8e0c798479f8ace498b410014
khaledibrahim1015 <ibrahimkhaled136@gmail.com>
2024-07-30 04:56:22 +0300

    adding ChangeTracket With EntityEntry and its state Feature

---
+++ EziOrm/ChangeTracker.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace EziOrm
+{
+    /// <summary>
+    /// This change tracker maintains a dictionary of tracked entities and their states.
+    //  The EntityEntry class captures the original values of an entity to detect changes.
+    //  DetectChanges method checks for modifications in unchanged entities.
+    //  This implementation allows for automatic change detection and more granular control over entity states.
+    /// </summary>
+    public class ChangeTracker
+    {
+        /// <summary>
+        /// on level of all entities store entity and its entityentry 
+        /// </summary>
+        private readonly Dictionary<object, EntityEntry> _trackedEntities = new Dictionary<object, EntityEntry>();
+
+        public void TrackEntity(object entity, EntityState state)
+        {
+
+            if (!_trackedEntities.TryGetValue(entity, out var enityEntry))
+            {
+                enityEntry = new EntityEntry(entity);
+                _trackedEntities[entity] = enityEntry;
+            }
+            enityEntry.State = state;
+        }
+
+        public EntityState GetEntityState(object entity)
+            => _trackedEntities.TryGetValue(entity, out var enityEntry) ? enityEntry.State : EntityState.Unchanged;
+
+        public IEnumerable<EntityEntry> GetEntityEntries() => _trackedEntities.Values;
+
+        public void DetectChanges()
+        {
+            foreach (EntityEntry entityEntry in _trackedEntities.Values)
+            {
+                if (entityEntry.State == EntityState.Unchanged )
+                    if (entityEntry.HasChanges())
+                        entityEntry.State = EntityState.Modified;
+            }
+        }
+
+
+    }
+}
---
+++ EziOrm/EntityEntry.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EziOrm
+{
+    /// <summary>
+    /// The EntityEntry class captures the original values of an entity to detect changes.
+    /// </summary>
+    public class EntityEntry
+    {
+        public object Entity { get; }
+        public EntityState State { get; set; }
+
+        /// <summary>
+        ///  on level of one entity to store prop name and its value 
+        /// </summary>
+        private readonly Dictionary<string, object?> _originalValues = new Dictionary<string, object?>();
+
+
+        public EntityEntry(object entity)
+        {
+            Entity = entity;
+            State = EntityState.Unchanged;
+            _originalValues = Entity
+                                    .GetType().GetProperties()
+                                    .ToDictionary(prop => prop.Name, prop => prop.GetValue(entity));
+        }
+
+        public bool HasChanges()
+        {
+
+            return Entity.GetType().GetProperties().Any(prop =>
+            {
+
+                var originalValue = _originalValues[prop.Name];
+                var currentValue = prop.GetValue(Entity);
+                return !Equals(currentValue, originalValue);
+
+            }
+            );
+        }
+
+
+
+
+
+
+    }
+}
